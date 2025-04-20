289f0e59fbf1c2fce9981448df556557b131297c
romnn <roman.dahm@gmx.de>
2025-04-20 18:02:18 +0200

    ci: mark the checked out repo as safe

--- release.sh
+++ release.sh
@@ -3,3 +3,6 @@
 echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | tee /etc/apt/sources.list.d/goreleaser.list
 apt update
 apt install -y git goreleaser mingw-w64
+
+# github actions requires to mark the current git repository as safe
+git config --global --add safe.directory ./
